ab1cdece2da6fa6e8d3161d4e02a4a38cf792593
executive8 <ofosuoffei8@gmail.com>
2022-12-14 16:18:25 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -22,20 +22,20 @@ else if (i == 98)
 {
 printf("%d\n", i);
 }
+}
+}
 else if (n >= 98)
 {
 for (j = n; j >= 98; j--)
 {
-if (j != 98)
-{
+if (j != 98){
 printf("%d, ", j);
 }
-else if (j == 98)
-{
+else if (j == 98){
 printf("%d\n", j);
 }
 }
 }
 }
-}
-}
+
+
